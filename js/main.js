6ea7eef81956b76f05e1bcdfde0fd0122e2ea049
saulmacflores <83150838+saulmacflores@users.noreply.github.com>
2022-04-29 21:39:30 -0600

    added

--- js/main.js
+++ js/main.js
@@ -1,23 +1,22 @@
 const links = [
-    {
-      label: "Week1 notes",
-      url: "week1/index.html"
-    },
-    {
-      label: "Week2 notes",
-      url: "week2/index.html"
-    }
-  ]
-  function loadIndex(){
-     const ol  = document.querySelector("#linksList");
-     links.forEach(link =>{
-         const item = document.createElement("li");
-         const href = document.createElement("a");
-         href.setAttribute("href", link.url);
-         href.innerHTML= link.label;
-
-         item.appendChild(href);
-         ol.appendChild(item);
-
-     })
+  {
+    label: "Week1 notes",
+    url: "week1/index.html"
+  },
+  {
+    label: "Week2 notes",
+    url: "week2/index.html"
   }
+]
+
+function loadIndex(){
+  const ol  = document.querySelector("#linksList");
+  links.forEach(link =>{
+    const item = document.createElement("li");
+    const href = document.createElement("a");
+    href.setAttribute("href", link.url);
+    href.innerHTML= link.label;
+    item.appendChild(href);
+    ol.appendChild(item);
+  })
+}
